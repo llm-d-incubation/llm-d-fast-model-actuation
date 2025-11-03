b5c6235877babbe9b1e90b69cb86322b87cbc940
Gloire Rubambiza <gloire@ibm.com>
2025-11-03 13:48:36 -0500

    Added functionality for pretty-printing of results.
    Also modified the REQUESTNAME var to REPLICASET.
    
    Signed-off-by: Gloire Rubambiza <gloire@ibm.com>

--- inference_server/benchmark/benchmark_base.py
+++ inference_server/benchmark/benchmark_base.py
@@ -117,7 +117,7 @@ class DualPodsBenchmark:
         :return: A string representing the path to the YAML file.
         """
         # Invoke the replacement with the unique replicaset name.
-        sed_cmd = "s#{REQUEST_NAME}#" + rs_name + "#"
+        sed_cmd = "s#${REPLICASET_NAME}#" + rs_name + "#"
         rs_name_yaml = rs_name + ".yaml"
         with Path(rs_name_yaml).open(mode="wb") as rs_yaml_fd:
             invoke_shell(
@@ -204,24 +204,56 @@ class DualPodsBenchmark:
             run["prv_time"] for run in success_runs if run["prv_time"] is not None
         ]
 
+        # Compute the number of pods awoken.
+        hits = len([run for run in success_runs if run["availability_mode"] == "Hit"])
+
         summary = {
-            "Total Runs": len(self.results),
-            "Successful Runs": len(success_runs),
-            "Failed Runs": len(self.results) - len(success_runs),
-            "Average Requester Time": (
-                sum(rq_times) / len(rq_times) if rq_times else None
-            ),
-            "Min Requester Time": min(rq_times) if rq_times else None,
-            "Max Requester Time": max(rq_times) if rq_times else None,
-            "Average Provider Time": (
-                sum(prv_times) / len(prv_times) if prv_times else None
-            ),
-            "Min Provider Time": min(prv_times) if prv_times else None,
-            "Max Provider Time": max(prv_times) if prv_times else None,
-            "All Results": self.results,
+            "total_runs": len(self.results),
+            "successful_runs": len(success_runs),
+            "hits": hits,
+            "hit_percent": int((hits / len(success_runs)) * 100),
+            "failed_runs": len(self.results) - len(success_runs),
+            "rq_min": min(rq_times) if rq_times else None,
+            "rq_max": max(rq_times) if rq_times else None,
+            "rq_avg": (sum(rq_times) / len(rq_times) if rq_times else None),
+            "prv_min": min(prv_times) if prv_times else None,
+            "prv_max": max(prv_times) if prv_times else None,
+            "prv_avg": (sum(prv_times) / len(prv_times) if prv_times else None),
+            "all_results": self.results,
         }
 
         return summary
+
+    def pretty_print_results(self):
+        """Log the results in a human readable format."""
+        summary = self.get_results()
+        total_runs = summary["total_runs"]
+        success_runs = summary["successful_runs"]
+        failed_runs = summary["failed_runs"]
+        hits = summary["hits"]
+        hit_percent = summary["hit_percent"]
+        rq_min = summary["rq_min"]
+        rq_max = summary["rq_max"]
+        rq_avg = summary["rq_avg"]
+        prv_min = summary["prv_min"]
+        prv_max = summary["prv_max"]
+        prv_avg = summary["prv_avg"]
+
+        run_str = (
+            "---------------------------------------------------------------------"
+        )
+        run_str += (
+            f"\n\nTotal Runs: {total_runs}\n" + f"Successful Runs: {success_runs}\n"
+        )
+        run_str += f"Failed Runs: {failed_runs}\n"
+        rq_stats = f"Requester Pods \n\tMin: {rq_min}s, \n\tMax: {rq_max}s"
+        rq_stats += f"\n\tAverage: {rq_avg}s\n"
+        prv_stats = f"Provider Pods \n\tMin: {prv_min}s, \n\tMax: {prv_max}s"
+        prv_stats += f"\n\tAverage: {prv_avg}s\n"
+        avail_stats = f"Hits: {hits}/{success_runs} ({hit_percent}%)\n"
+
+        summary_str = "".join([run_str, rq_stats, prv_stats, avail_stats])
+        self.logger.info(summary_str)
 
     def cleanup_resources(self):
         """Clean up any remaining resources in kind or remote cluster."""
@@ -237,19 +269,15 @@ if __name__ == "__main__":
     # kind_benchmark = DualPodsBenchmark(
     #    "kind", cluster_name="fmatest", log_output_file=kind_log_path
     # )
-    # sim_log_path = "sim_logger.log"
-    # sim_benchmark = DualPodsBenchmark("simulated", log_output_file=sim_log_path)
-    remote_log_path = "remote_logger.log"
-    remote_benchmark = DualPodsBenchmark("remote", log_output_file=remote_log_path)
+    sim_log_path = "sim_logger.log"
+    sim_benchmark = DualPodsBenchmark("simulated", log_output_file=sim_log_path)
+    # remote_log_path = "remote_logger.log"
+    # remote_benchmark = DualPodsBenchmark("remote", log_output_file=remote_log_path)
     # all_benchmarks = [sim_benchmark, kind_benchmark]
-    all_benchmarks = [remote_benchmark]
+    all_benchmarks = [sim_benchmark]
 
     # Run example benchmarks
     for benchmark in all_benchmarks:
         results = benchmark.run_benchmark(4)
-        for result in results:
-            print(f"\nIteration: {result['iteration']}")
-            print(f"Requester Ready: {result['rq_time']}")
-            print(f"Provider: {result['prv_time']}")
-            print(f"Availability Mode: {result['availability_mode']}\n")
+        benchmark.pretty_print_results()
         # benchmark.run_benchmark("Introducing New Variant", 3, **kwargs)
