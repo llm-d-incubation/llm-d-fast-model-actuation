c70d3f56b126af1b3db385f57e59aa0e9ff6e9cd
Mike Spreitzer <mspreitz@us.ibm.com>
2025-12-05 10:37:22 -0500

    Added explanation of make failures into verification script
    Signed-off-by: Mike Spreitzer <mspreitz@us.ibm.com>

--- hack/verify-idl-consumption.sh
+++ hack/verify-idl-consumption.sh
@@ -6,7 +6,8 @@
 # This script checks that the IDL processing is correct.
 
 rm -rf config/crd $(find api -name "zz_generated*")
-make manifests generate
+make manifests || { echo "Failed to generate CRDs"; exit 1; }
+make generate || { echo "Failed to generate auxiliary API code"; exit 1; }
 if ! git status --porcelain=v2 | wc -l | grep -qw 0; then
     git status
     exit 1
