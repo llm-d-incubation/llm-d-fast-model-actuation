4ac239adae6d82b2a884a819c84ab243112101ac
王夕宁 <osswangxining@163.com>
2025-12-25 12:09:07 +0800

    Add log info for NodeLauncherKey map

--- pkg/controller/launcher-pool/populator.go
+++ pkg/controller/launcher-pool/populator.go
@@ -252,6 +252,7 @@ func (item lppItem) process(ctx context.Context, ctl *controller) (error, bool) 
 		}
 	}
 
+	logger.Info("Final population policy", "policy", mapToString(populationPolicy))
 	// Adjust launcher pods according to final requirements
 	if err := ctl.reconcileAllLaunchers(ctx, populationPolicy); err != nil {
 		logger.Error(err, "Failed to reconcile launchers")
