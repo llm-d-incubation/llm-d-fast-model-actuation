fe4472ade1ab0428242477f2ac00bab6b9144dbc
王夕宁 <osswangxining@163.com>
2026-01-06 16:01:52 +0800

    Rebase

--- pkg/common/flags.go
+++ pkg/common/flags.go
@@ -34,30 +34,9 @@ func AddKubernetesClientFlags(flags pflag.FlagSet, loadingRules *clientcmd.Clien
 		return
 	}
 
-	// Check if the flag already exists to avoid duplicate definition
-	if f := flags.Lookup("kubeconfig"); f != nil {
-		loadingRules.ExplicitPath = f.Value.String()
-	} else {
-		flags.StringVar(&loadingRules.ExplicitPath, "kubeconfig", loadingRules.ExplicitPath, "Path to the kubeconfig file to use")
-	}
-	if f := flags.Lookup("context"); f != nil {
-		overrides.CurrentContext = f.Value.String()
-	} else {
-		flags.StringVar(&overrides.CurrentContext, "context", overrides.CurrentContext, "The name of the kubeconfig context to use")
-	}
-	if f := flags.Lookup("user"); f != nil {
-		overrides.Context.AuthInfo = f.Value.String()
-	} else {
-		flags.StringVar(&overrides.Context.AuthInfo, "user", overrides.Context.AuthInfo, "The name of the kubeconfig user to use")
-	}
-	if f := flags.Lookup("cluster"); f != nil {
-		overrides.Context.Cluster = f.Value.String()
-	} else {
-		flags.StringVar(&overrides.Context.Cluster, "cluster", overrides.Context.Cluster, "The name of the kubeconfig cluster to use")
-	}
-	if f := flags.Lookup("namespace"); f != nil {
-		overrides.Context.Namespace = f.Value.String()
-	} else {
-		flags.StringVarP(&overrides.Context.Namespace, "namespace", "n", overrides.Context.Namespace, "The name of the Kubernetes Namespace to work in (NOT optional)")
-	}
+	flags.StringVar(&loadingRules.ExplicitPath, "kubeconfig", loadingRules.ExplicitPath, "Path to the kubeconfig file to use")
+	flags.StringVar(&overrides.CurrentContext, "context", overrides.CurrentContext, "The name of the kubeconfig context to use")
+	flags.StringVar(&overrides.Context.AuthInfo, "user", overrides.Context.AuthInfo, "The name of the kubeconfig user to use")
+	flags.StringVar(&overrides.Context.Cluster, "cluster", overrides.Context.Cluster, "The name of the kubeconfig cluster to use")
+	flags.StringVarP(&overrides.Context.Namespace, "namespace", "n", overrides.Context.Namespace, "The name of the Kubernetes Namespace to work in (NOT optional)")
 }
