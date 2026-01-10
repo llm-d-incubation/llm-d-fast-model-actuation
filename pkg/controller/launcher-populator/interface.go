7eee2a41826e1426844f76bb08ebfd1096dcf4a4
王夕宁 <osswangxining@163.com>
2026-01-10 11:28:57 +0800

    Refine the launcher pod template

--- pkg/controller/launcher-populator/interface.go
+++ pkg/controller/launcher-populator/interface.go
@@ -26,4 +26,8 @@ const (
 	LauncherConfigNameLabelKey = "dual-pods.llm-d.ai/launcher-config-name"
 
 	NodeNameLabelKey = "node.kubernetes.io/node-name"
+
+	PortDiscoveryAnnotationKey = "inference.networking.x-k8s.io/port-discovery"
+
+	PortDiscoveryAnnotationEmptyValue = "[]"
 )
