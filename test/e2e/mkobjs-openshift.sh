38258cffc14152cf8a2dc1f8aa8d47a1506537c4
Mike Spreitzer <mspreitz@us.ibm.com>
2026-08-28 15:32:42 -0400

    Guess mitigation for vLLM crash at startup
    Signed-off-by: Mike Spreitzer <mspreitz@us.ibm.com>

--- test/e2e/mkobjs-openshift.sh
+++ test/e2e/mkobjs-openshift.sh
@@ -215,7 +215,7 @@ spec:
             value: "/tmp"
           resources:
             limits:
-              ephemeral-storage: "4.5Gi"
+              ephemeral-storage: "5Gi"
 ---
 apiVersion: fma.llm-d.ai/v1alpha1
 kind: LauncherPopulationPolicy
